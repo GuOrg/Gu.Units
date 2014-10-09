f83add3997be33578fdf990b75bda95b73d739c9
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-10 01:27:26 +0200

    Default value if parameter is missing for UnitValue.tt

---
+++ GeneratorBox.Generator/Descriptors/UnitMetaData.cs
@@ -0,0 +1,30 @@
+﻿namespace GeneratorBox.Generator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [Serializable]
+    public class UnitMetaData : MarshalByRefObject
+    {
+        private UnitMetaData()
+        {
+            Units = new List<TemplateType>();
+        }
+        public UnitMetaData(string baseType, string ns, string className, params string[] units)
+            : this()
+        {
+            BaseType = new TemplateType(baseType);
+            Namespace = ns;
+            ClassName = new TemplateType(className);
+            Units = units.Select(x => new TemplateType(x)).ToArray();
+        }
+        public TemplateType BaseType { get; set; }
+
+        public string Namespace { get; set; }
+
+        public TemplateType ClassName { get; set; }
+
+        public IList<TemplateType> Units { get; set; }
+    }
+}
