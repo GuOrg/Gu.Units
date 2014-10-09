923c5e499d161bbd6609a681ad4f284817c96e63
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-09 21:41:17 +0200

    Genetarting field now

--- GeneratorBox/Units/Unit1.generated.cs
+++ GeneratorBox/Units/Unit1.generated.cs
@@ -4,9 +4,19 @@
 namespace GeneratorBox.Units
 {
     using System;
+    using System.Globalization;
+    using System.Xml;
+    using System.Xml.Linq;
+    using System.Xml.Schema;
+    using System.Xml.Serialization;
 
-    public partial struct Unit1
+    /// <summary>
+    /// An Unit1
+    /// </summary>
+    [Serializable]
+    public partial struct Unit1 : IComparable<Unit1>, IEquatable<Unit1>, IFormattable, IXmlSerializable, IUnitValue
     {
+
         public int Prop1 { get; set; }
     }
 }
