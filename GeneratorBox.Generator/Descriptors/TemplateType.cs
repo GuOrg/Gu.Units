cadd1b0992e28eaeb48fd0f56c51b219cfb45fd6
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-09 23:48:42 +0200

    Writing constructors now

---
+++ GeneratorBox.Generator/Descriptors/TemplateType.cs
@@ -0,0 +1,22 @@
+﻿namespace GeneratorBox.Generator
+{
+    using System;
+
+    public class TemplateType : MarshalByRefObject
+    {
+        private TemplateType()
+        {
+            
+        }
+        public TemplateType(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+        public string ParameterName { get { return Char.ToLower(Name[0]) + Name.Substring(1); } }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ GeneratorBox/Units/Length.generated.cs
@@ -0,0 +1,38 @@
+﻿// <summary file="Length.generated.cs" company="...">
+// some text...
+// </summary>
+namespace GeneratorBox.Units
+{
+    using System;
+    using System.Globalization;
+    using System.Xml;
+    using System.Xml.Linq;
+    using System.Xml.Schema;
+    using System.Xml.Serialization;
+
+	/// <summary>
+    /// An Length
+    /// </summary>
+    [Serializable]
+	public partial struct Length : IComparable<Length>, IEquatable<Length>, IFormattable, IXmlSerializable, IUnitValue
+	{
+	    /// <summary>
+        /// The value in <see cref="T:GeneratorBox.Units.Meters"/>.
+        /// </summary>
+		public readonly Meters Meters;
+		
+		private Length(double meters)
+        {
+            Meters = meters;
+        }
+
+		/// <summary>
+        /// Initializes a new instance of <see cref="T:GeneratorBox.Units.Meters"/>.
+        /// </summary>
+        /// <param name="meters"></param>
+        /// <param name="unit"></param>
+        public Angle(double meters, Meters unit)
+        {
+            Meters = meters;
+        }
+		
