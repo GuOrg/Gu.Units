96e451efa7b184c512b068aa5eaf4ae6b2fa18be
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-09 20:51:28 +0200

    Reading from outside the template now

---
+++ GeneratorBox.Generator/Settings.cs
@@ -0,0 +1,48 @@
+﻿namespace GeneratorBox.Generator
+{
+    using System.Collections.Generic;
+
+    public class Settings
+    {
+        static Settings()
+        {
+            Descriptors = new List<UnitValueDescriptor>
+                              {
+                                  new UnitValueDescriptor
+                                      {
+                                          ClassName = "Unit1",
+                                          Namespace = "GeneratorBox.Units",
+                                          Properties =
+                                              new[]
+                                                  {
+                                                      new Property
+                                                          {
+                                                              PropertyName ="Prop1",
+                                                              ReturnType = "int"
+                                                          }
+                                                  }
+                                      },
+                                      new UnitValueDescriptor
+                                      {
+                                          ClassName = "Unit2",
+                                          Namespace = "GeneratorBox.Units",
+                                          Properties =
+                                              new[]
+                                                  {
+                                                      new Property
+                                                          {
+                                                              PropertyName ="Prop1",
+                                                              ReturnType = "int"
+                                                          }
+                                                  }
+                                      }
+
+                              };
+        }
+        public Settings()
+        {
+
+        }
+        public static List<UnitValueDescriptor> Descriptors { get; set; }
+    }
+}
---
+++ GeneratorBox/Units/Unit1.generated.cs
@@ -0,0 +1,12 @@
+﻿// <summary file="Unit1.generated.cs" company="...">
+// some text...
+// </summary>
+namespace GeneratorBox.Units
+{
+    using System;
+
+    public class Unit1
+    {
+        public int Prop1 { get; set; }
+    }
+}
