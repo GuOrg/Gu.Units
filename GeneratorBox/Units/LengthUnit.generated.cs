7549bd8fd23212d2fc17a26ed4fffb4ad06ba0db
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-10 22:02:16 +0200

    Squash -> bed

---
+++ GeneratorBox/Units/LengthUnit.generated.cs
@@ -0,0 +1,13 @@
+﻿
+namespace GeneratorBox
+{
+    public static class LengthUnit
+    {
+        public static readonly Meters Meters = new Meters();
+        public static readonly Meters m = Meters;
+        public static readonly Millimeters Millimeters = new Millimeters();
+        public static readonly Millimeters mm = Millimeters;
+        public static readonly Centimeters Centimeters = new Centimeters();
+        public static readonly Centimeters cm = Centimeters;
+    }
+}
