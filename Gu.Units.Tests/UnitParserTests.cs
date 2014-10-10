2858fc30fcc2bb46a8445800e60fa33420e9c86f
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-11 00:15:27 +0200

    Added some more failing testcases

---
+++ Gu.Units.Tests/UnitParserTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Gu.Units.Tests
+{
+    using NUnit.Framework;
+
+    public class UnitParserTests
+    {
+        [TestCase("1m", 1)]
+        [TestCase("-1m", 1)]
+        [TestCase("1e3m", 1e3)]
+        [TestCase("1e+3m", 1e+3)]
+        [TestCase("1e-3m", 1e-3)]
+        [TestCase(" 1m", 1)]
+        [TestCase("1 m", 1)]
+        [TestCase("1m ", 1)]
+        public void ParseLength(string s, double expected)
+        {
+            var length = UnitParser.Parse<ILengthUnit, Length>(s, Length.From);
+            Assert.AreEqual(expected, length.Meters);
+        }
+    }
+}
