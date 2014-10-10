f2524d02b4e909183859796d43c5f0de75485d9a
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-11 00:15:27 +0200

    Added some more failing testcases

--- Gu.Units/Units/UnitParser.cs
+++ Gu.Units/Units/UnitParser.cs
@@ -1,26 +1,30 @@
 namespace Gu.Units
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Globalization;
     using System.Text.RegularExpressions;
 
-    public class UnitParser
+    public static class UnitParser
     {
         public const string UnitValuePattern = @"^(?: *)(?<Value>[+-]?\d+([eE][+-]\d+)?([.,]\d+)?) *(?<Unit>.+) *$";
         public const string DoublePattern = @"[+-]?\d+([eE][+-]\d+)?([.,]\d+)?";
-        public static TValue Parse<TUnit, TValue>(string s, Func<double, TUnit, TValue> creator) 
-            where TUnit : IUnit 
+        private static readonly ConcurrentDictionary<Type, string> Symbols = new ConcurrentDictionary<Type, string>();
+        public static TValue Parse<TUnit, TValue>(string s, Func<double, TUnit, TValue> creator)
+            where TUnit : IUnit
             where TValue : IUnitValue
         {
             Match match = Regex.Match(s, UnitValuePattern);
             double d = ParseDouble(match.Groups["Value"].Value);
             var unit = ParseUnit<TUnit>(match.Groups["Unit"].Value);
-            return creator(d, (TUnit)unit);
+            return creator(d, unit);
         }
 
         public static TUnit ParseUnit<TUnit>(string s)
         {
             var trim = s.Trim();
+            var type = typeof(TUnit);
+            var symbol = Symbols.GetOrAdd(type, t => t.Symbol());
             throw new NotImplementedException("message");
         }
 
@@ -28,5 +32,10 @@ namespace Gu.Units
         {
             return double.Parse(s.Replace(',', '.'), CultureInfo.InvariantCulture);
         }
-    }
+
+        private static string Symbol(this Type t)
+        {
+            throw new NotImplementedException("message");
+        }
+     }
 }
