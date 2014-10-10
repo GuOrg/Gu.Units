59c5b960816b08c02bd8d496a3125e2ad767869a
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-10 23:30:33 +0200

    Time to write tests now

---
+++ Gu.Units.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gu.Units.Tests
+{
+    public class Class1
+    {
+    }
+}
