597340ef1f7d46690ab5f2a5677a499ddbd7812d
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-11 18:57:20 +0200

    Fleshed out Parsing, should write better exception messages

--- Gu.Units/Utils/Parser.cs
+++ Gu.Units/Utils/Parser.cs
@@ -9,8 +9,10 @@ namespace Gu.Units
 
     public static class Parser
     {
-        public static readonly string DoublePattern = @"[+-]?\d*(?:[.,]\d+)?(?:[eE][+-]?\d+)?";
-        public static readonly string UnitValuePattern = string.Format(@"^(?: *)(?<Value>{0}) *(?<Unit>.+) *$", DoublePattern);
+        public static readonly string DoublePointPattern = @"[+-]?\d*(?:.\d+)?(?:[eE][+-]?\d+)?";
+        public static readonly string DoubleCommaPattern = @"[+-]?\d*(?:,\d+)?(?:[eE][+-]?\d+)?";
+        public static readonly string UnitValuePointPattern = string.Format(@"^(?: *)(?<Value>{0}) *(?<Unit>.+) *$", DoublePointPattern);
+        public static readonly string UnitValueCommaPattern = string.Format(@"^(?: *)(?<Value>{0}) *(?<Unit>.+) *$", DoubleCommaPattern);
         private static readonly ConcurrentDictionary<Type, IUnit[]> Symbols = new ConcurrentDictionary<Type, IUnit[]>();
         public static TValue Parse<TUnit, TValue>(string s, Func<double, TUnit, TValue> creator, IFormatProvider provider = null)
             where TUnit : IUnit
@@ -20,10 +22,39 @@ namespace Gu.Units
             {
                 provider = CultureInfo.InvariantCulture;
             }
-            Match match = Regex.Match(s, UnitValuePattern);
-            double d = ParseDouble(match.Groups["Value"].Value, provider);
-            var unit = ParseUnit<TUnit>(match.Groups["Unit"].Value);
-            return creator(d, unit);
+            try
+            {
+                var format = (NumberFormatInfo)provider.GetFormat(typeof(NumberFormatInfo));
+                var separator = format.NumberDecimalSeparator;
+                Match match;
+                if (separator == ".")
+                {
+                    if (s.IndexOf(',') >= 0)
+                    {
+                        throw new ArgumentException("The given string does not match the culture");
+                    }
+                    match = Regex.Match(s, UnitValuePointPattern);
+                }
+                else if (separator == ",")
+                {
+                    if (s.IndexOf('.') >= 0)
+                    {
+                        throw new ArgumentException("The given string does not match the culture");
+                    }
+                    match = Regex.Match(s, UnitValuePointPattern);
+                }
+                else
+                {
+                    throw new ArgumentException("Cannot format the given culture", "provider");
+                }
+                double d = ParseDouble(match.Groups["Value"].Value, provider);
+                var unit = ParseUnit<TUnit>(match.Groups["Unit"].Value);
+                return creator(d, unit);
+            }
+            catch (Exception e)
+            {
+                throw new FormatException("Could not parse the unit value from: " + s, e);
+            }
         }
 
         public static TUnit ParseUnit<TUnit>(string s)
