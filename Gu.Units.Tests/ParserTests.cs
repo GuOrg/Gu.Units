b28f11ac485205523c37ae68fdd426cebbd8dbb8
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-11 17:20:55 +0200

    Added test for double pattern

--- Gu.Units.Tests/ParserTests.cs
+++ Gu.Units.Tests/ParserTests.cs
@@ -1,49 +1,52 @@
 ﻿namespace Gu.Units.Tests
 {
+    using System.Globalization;
+    using System.Text.RegularExpressions;
     using NUnit.Framework;
 
     public class ParserTests
     {
-        [TestCase("1m", 1)]
+        [TestCase("1m", new[] { "sv-se", "en-us" }, 1)]
         [TestCase("-1m", -1)]
-        [TestCase("1.2m", 1.2)]
-        [TestCase("1,2m", 1.2)]
-        [TestCase("-1m", -1)]
-        [TestCase("1e3m", 1e3)]
-        [TestCase("-1e3m", -1e3)]
-        [TestCase("1e+3m", 1e+3)]
-        [TestCase("1E+3m", 1E+3)]
-        [TestCase("-1e-3m", -1e-3)]
-        [TestCase(" 1m", 1)]
-        [TestCase("1m ", 1)]
-        [TestCase("1 m", 1)]
-        [TestCase(" 1 m", 1)]
-        [TestCase("1 m ", 1)]
-        [TestCase(" 1 m ", 1)]
-        [TestCase(" 1   m   ", 1)]
-        [TestCase("1m ", 1)]
-        [TestCase("1mm", 1e-3)]
-        [TestCase("1cm", 1e-2)]
-        public void ParseLength(string s, double expected)
+        [TestCase("1.2m", new[] { "en-us" }, 1.2)]
+        [TestCase("1,2m", new[] { "sv-se" }, 1.2)]
+        [TestCase("-1m", new[] { "sv-se", "en-us" }, -1)]
+        [TestCase("1e3m", new[] { "sv-se", "en-us" }, 1e3)]
+        [TestCase("1E3m", new[] { "sv-se", "en-us" }, 1e3)]
+        [TestCase("1e+3m", new[] { "sv-se", "en-us" }, 1e+3)]
+        [TestCase("1E+3m", new[] { "sv-se", "en-us" }, 1E+3)]
+        [TestCase("1.2e-3m", new[] { "en-us" }, 1.2e-3)]
+        [TestCase("1.2E-3m", new[] { "en-us" }, 1.2e-3)]
+        [TestCase(" 1m", new[] { "sv-se", "en-us" }, 1)]
+        [TestCase("1 m", new[] { "sv-se", "en-us" }, 1)]
+        [TestCase("1m ", new[] { "sv-se", "en-us" }, 1)]
+        [TestCase("1mm", new[] { "sv-se", "en-us" }, 1e-3)]
+        [TestCase("1cm", new[] { "sv-se", "en-us" }, 1e-2)]
+        public void ParseLength(string s, string[] cultures, double expected)
         {
-            var length = Parser.Parse<ILengthUnit, Length>(s, Length.From);
-            Assert.AreEqual(expected, length.Meters);
+            foreach (var culture in cultures)
+            {
+                var cultureInfo = CultureInfo.GetCultureInfo(culture);
+                var length = Parser.Parse<ILengthUnit, Length>(s, Length.From, cultureInfo);
+                Assert.AreEqual(expected, length.Meters);
+            }
         }
 
-        [TestCase("1m", 1)]
-        public void ParseMeters(string s, double expected)
+        [TestCase("1", 1)]
+        [TestCase(".1", .1)]
+        [TestCase("1.2", 1.2)]
+        [TestCase("1.2E+3", 1.2E+3)]
+        [TestCase("1.2e+3", 1.2E+3)]
+        [TestCase("1.2E3", 1.2E3)]
+        [TestCase("1.2e3", 1.2E3)]
+        [TestCase("1.2E-3", 1.2E-3)]
+        [TestCase("1.2e-3", 1.2E-3)]
+        public void DoublePattern(string s, double expected)
         {
-            var length = UnitParser.Parse<ILengthUnit, Length>(s, Length.From);
-            Assert.AreEqual(expected, length.Meters);
+            Assert.IsTrue(Regex.IsMatch(s, Parser.DoublePattern));
+            Assert.AreEqual(expected, double.Parse(s, CultureInfo.InvariantCulture));
         }
-
-        [TestCase("1cm", 1)]
-        public void ParseCentimeters(string s, double expected)
-        {
-            var length = UnitParser.Parse<ILengthUnit, Length>(s, Length.From);
-            Assert.AreEqual(expected, length.Centimeters);
-        }
-
+        
         [TestCase("1mm", 1)]
         public void ParseMillimeters(string s, double expected)
         {
--- Gu.Units/Utils/Parser.cs
+++ Gu.Units/Utils/Parser.cs
@@ -9,15 +9,19 @@ namespace Gu.Units
 
     public static class Parser
     {
-        public static readonly string DoublePattern = @"[+-]?\d+(?:[.,]\d+)?(?:[eE][+-]?\d+)?";
+        public static readonly string DoublePattern = @"[+-]?\d*(?:[.,]\d+)?(?:[eE][+-]?\d+)?";
         public static readonly string UnitValuePattern = string.Format(@"^(?: *)(?<Value>{0}) *(?<Unit>.+) *$", DoublePattern);
         private static readonly ConcurrentDictionary<Type, IUnit[]> Symbols = new ConcurrentDictionary<Type, IUnit[]>();
-        public static TValue Parse<TUnit, TValue>(string s, Func<double, TUnit, TValue> creator)
+        public static TValue Parse<TUnit, TValue>(string s, Func<double, TUnit, TValue> creator, IFormatProvider provider = null)
             where TUnit : IUnit
             where TValue : IUnitValue
         {
+            if (provider == null)
+            {
+                provider = CultureInfo.InvariantCulture;
+            }
             Match match = Regex.Match(s, UnitValuePattern);
-            double d = ParseDouble(match.Groups["Value"].Value);
+            double d = ParseDouble(match.Groups["Value"].Value, provider);
             var unit = ParseUnit<TUnit>(match.Groups["Unit"].Value);
             return creator(d, unit);
         }
@@ -30,9 +34,9 @@ namespace Gu.Units
             return (TUnit)symbols.Single(x => x.Symbol == trim);
         }
 
-        public static double ParseDouble(string s)
+        public static double ParseDouble(string s, IFormatProvider provider)
         {
-            return double.Parse(s.Replace(',', '.'), CultureInfo.InvariantCulture);
+            return double.Parse(s, provider);
         }
 
         private static IUnit[] Symbol(this Type t)
