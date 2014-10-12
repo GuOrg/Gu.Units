9066d0441b97cde51297a88e78565c9c0f048d33
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-12 16:48:04 +0200

    Starting with arithmetics now. Simplest possible.

--- Gu.Units/Interfaces/IQuantity.cs
+++ Gu.Units/Interfaces/IQuantity.cs
@@ -1,6 +1,67 @@
 ﻿namespace Gu.Units
 {
+    using System;
+
     public interface IQuantity
+    {
+        Type GetType();
+    }
+
+    public interface IQuantity<out T> : IQuantity
+        where T : IPower<IUnit>
+    {
+    }
+
+    public struct Quantity<T> : IQuantity<T> where T : IPower<IUnit>
+    {
+    }
+
+    public interface IQuantity<T1,T2> : IQuantity
+        where T1 : IPower<IUnit>
+        where T2 : IPower<IUnit>
+    {
+    }
+
+    public interface IQuantity<T1, T2, T3> : IQuantity
+        where T1 : IPower<IUnit>
+        where T2 : IPower<IUnit>
+        where T3 : IPower<IUnit>
+    {
+    }
+
+    public interface IQuantity<T1, T2, T3, T4> : IQuantity
+        where T1 : IPower<IUnit>
+        where T2 : IPower<IUnit>
+        where T3 : IPower<IUnit>
+        where T4 : IPower<IUnit>
+    {
+    }
+
+    public interface IPower<out T> where T : IUnit
+    {
+    }
+
+    public interface I1<out T> : IPower<T> where T : IUnit
+    {
+    }
+
+    public interface I2<out T> : IPower<T> where T : IUnit
+    {
+    }
+
+    public interface I3<out T> : IPower<T> where T : IUnit
+    {
+    }
+
+    public interface INeg1<out T> : IPower<T> where T : IUnit
+    {
+    }
+
+    public interface INeg2<out T> : IPower<T> where T : IUnit
+    {
+    }
+
+    public interface INeg3<out T> : IPower<T> where T : IUnit
     {
     }
 }
---
+++ Gu.Units/Utils/Arithmetics.cs
@@ -0,0 +1,11 @@
+﻿namespace Gu.Units.Utils
+{
+    using System;
+    public static class Arithmetics
+    {
+        public static IQuantity Multiply(IQuantity<IPower<ILengthUnit>> left, IQuantity<IPower<ILengthUnit>> right) 
+        {
+            return new Quantity<I2<ILengthUnit>>();
+        }
+    }
+}
