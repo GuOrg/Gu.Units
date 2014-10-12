fc31b1e2ac66dde3330a10de97f555be677832f1
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-12 16:48:04 +0200

    Starting with arithmetics now. Simplest possible.

---
+++ Gu.Units.Tests/ArithmeticsTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Gu.Units.Tests
+{
+    using Moq;
+    using NUnit.Framework;
+    using Utils;
+
+    public class ArithmeticsTests
+    {
+        [Test]
+        public void MultiplyTest()
+        {
+            var lengthMock1 = new Mock<IQuantity<I1<ILengthUnit>>>();
+            lengthMock1.Setup(x => x.GetType()).Returns(typeof (IQuantity<I1<ILengthUnit>>));
+            var lengthMock2 = new Mock<IQuantity<I1<ILengthUnit>>>();
+            lengthMock2.Setup(x => x.GetType()).Returns(typeof(IQuantity<I1<ILengthUnit>>));
+
+            var multiply = Arithmetics.Multiply(lengthMock1.Object, lengthMock2.Object);
+            Assert.IsInstanceOf<IQuantity<I2<ILengthUnit>>>(multiply);
+        }
+    }
+}
