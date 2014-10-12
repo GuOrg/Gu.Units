92a820592d18f2373f21186ebe3ec4425175f72b
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-12 17:04:28 +0200

    Iteration 1

--- Gu.Units/Utils/Arithmetics.cs
+++ Gu.Units/Utils/Arithmetics.cs
@@ -3,9 +3,12 @@
     using System;
     public static class Arithmetics
     {
-        public static IQuantity Multiply(IQuantity<IPower<ILengthUnit>> left, IQuantity<IPower<ILengthUnit>> right) 
+        public static IQuantity Multiply<TPowerLeft, TPowerRight, TUnit>(IQuantity<TPowerLeft, TUnit> left, IQuantity<TPowerRight, TUnit> right)
+            where TPowerLeft : IPower<TUnit>
+            where TPowerRight : IPower<TUnit>
+            where TUnit : IUnit
         {
-            return new Quantity<I2<ILengthUnit>>();
+            return new Quantity<I2<TUnit>, TUnit>();
         }
     }
 }
