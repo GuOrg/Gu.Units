f282c625294c67d7226ad556ff5df8a68571bd40
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-13 22:48:33 +0200

    squash -> home

---
+++ Gu.Units.Generator/WpfStuff/UnitCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace Gu.Units.Generator.WpfStuff
+{
+    using System.Collections.ObjectModel;
+
+    public class UnitCollection<T> : ObservableCollection<T>
+        where T : IUnit
+    {
+        protected override void InsertItem(int index, T item)
+        {
+
+            base.InsertItem(index, item);
+        }
+    }
+}
