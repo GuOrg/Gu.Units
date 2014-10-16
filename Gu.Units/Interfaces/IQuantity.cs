4909d672d67194d062e1930d2a2ab9de4654d3e1
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-16 13:51:00 +0200

    squash

--- Gu.Units/Interfaces/IQuantity.cs
+++ Gu.Units/Interfaces/IQuantity.cs
@@ -8,15 +8,15 @@
         double SiValue { get; }
     }
 
-    public interface IQuantity<out TPower, out TUnit> : IQuantity
-        where TPower : IPower<TUnit>
+    public interface IQuantity<TUnit, TPower> : IQuantity
         where TUnit : IUnit
+        where TPower : IPower
     {
     }
 
-    public struct Quantity<TPower, TUnit> : IQuantity<TPower, TUnit>
-        where TPower : IPower<TUnit>
+    public struct Quantity<TUnit, TPower> : IQuantity<TUnit, TPower>
         where TUnit : IUnit
+        where TPower : IPower
     {
         private readonly double _siValue;
         public Quantity(double siValue)
@@ -29,54 +29,47 @@
         }
     }
 
-    public interface IQuantity<TPower1, TUnit1, TPower2, TUnit2> : IQuantity
-        where TPower1 : IPower<TUnit1>
-        where TPower2 : IPower<TUnit2>
+    public interface IQuantity<TUnit1, TPower1, TUnit2, TPower2> : IQuantity
         where TUnit1 : IUnit
+        where TPower1 : IPower
         where TUnit2 : IUnit
+        where TPower2 : IPower
     {
     }
 
-    public interface IQuantity<T1, T2, T3> : IQuantity
-        where T1 : IPower<IUnit>
-        where T2 : IPower<IUnit>
-        where T3 : IPower<IUnit>
+    public interface IPower
     {
     }
 
-    //public interface IQuantity<T1, T2, T3, T4> : IQuantity
-    //    where T1 : IPower<IUnit>
-    //    where T2 : IPower<IUnit>
-    //    where T3 : IPower<IUnit>
-    //    where T4 : IPower<IUnit>
-    //{
-    //}
-
-    public interface IPower<out T> where T : IUnit
+    public interface I1 : IPower
     {
     }
 
-    public interface I1<out T> : IPower<T> where T : IUnit
+    public interface I2 : IPower
     {
     }
 
-    public interface I2<out T> : IPower<T> where T : IUnit
+    public interface I3 : IPower
     {
     }
 
-    public interface I3<out T> : IPower<T> where T : IUnit
+    public interface I4 : IPower
     {
     }
 
-    public interface INeg1<out T> : IPower<T> where T : IUnit
+    public interface INeg1 : IPower
     {
     }
 
-    public interface INeg2<out T> : IPower<T> where T : IUnit
+    public interface INeg2 : IPower
     {
     }
 
-    public interface INeg3<out T> : IPower<T> where T : IUnit
+    public interface INeg3 : IPower
+    {
+    }
+
+    public interface INeg4 : IPower
     {
     }
 }
