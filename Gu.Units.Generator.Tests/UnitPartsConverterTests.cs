3c7474ef606c5fcf9fd2e6e2edfbddcafc9fe212
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-17 17:50:56 +0200

    squash amend :)

--- Gu.Units.Generator.Tests/UnitPartsConverterTests.cs
+++ Gu.Units.Generator.Tests/UnitPartsConverterTests.cs
@@ -15,6 +15,9 @@
             Assert.IsTrue(converter.CanConvertFrom(null, typeof(string)));
             var parts = (UnitParts)converter.ConvertFrom(null, null, data.Value);
             CollectionAssert.AreEquivalent(data.Units, parts);
+            var convertTo = converter.ConvertTo(null, null, parts, typeof(string));
+            Assert.AreEqual(data.Formatted, convertTo);
+            
             var uiName = parts.Expression;
             Assert.AreEqual(data.Value.Replace(" ", ""), uiName.Replace('⋅', '*').Replace(" ", ""));
         }
@@ -32,36 +35,44 @@
 
         private readonly List<Data> _datas = new List<Data>
                                         {
-                                            new Data("m^2", new UnitAndPower(Metres, 2)),
-                                            new Data("m²", new UnitAndPower(Metres, 2)),
-                                            new Data("kg⋅m/s²",new UnitAndPower(Kilograms, 1),new UnitAndPower(Metres, 1),new UnitAndPower(Seconds, -2)),
-                                            new Data("kg⋅m⋅s⁻²",new UnitAndPower(Kilograms, 1),new UnitAndPower(Metres, 1),new UnitAndPower(Seconds, -2)),
-                                            new Data("kg*m/s²",new UnitAndPower(Kilograms, 1),new UnitAndPower(Metres, 1),new UnitAndPower(Seconds, -2)),
-                                            new Data("m/s", new UnitAndPower(Metres,1), new UnitAndPower(Seconds,-1)),
-                                            new Data("m⋅s^-1", new UnitAndPower(Metres,1), new UnitAndPower(Seconds,-1)),
-                                            new Data("m⋅s⁻¹", new UnitAndPower(Metres,1), new UnitAndPower(Seconds,-1)),
-                                            new Data("1/s", new UnitAndPower(Seconds,-1)),
-                                            new Data("s^-1", new UnitAndPower(Seconds,-1))
+                                            new Data("m^2","m²", new UnitAndPower(Metres, 2)),
+                                            new Data("m²","m²", new UnitAndPower(Metres, 2)),
+                                            new Data("kg⋅m/s²","kg⋅m⋅s⁻²",new UnitAndPower(Kilograms, 1),new UnitAndPower(Metres, 1),new UnitAndPower(Seconds, -2)),
+                                            new Data("kg⋅m⋅s⁻²","kg⋅m⋅s⁻²",new UnitAndPower(Kilograms, 1),new UnitAndPower(Metres, 1),new UnitAndPower(Seconds, -2)),
+                                            new Data("kg*m/s²","kg⋅m⋅s⁻²",new UnitAndPower(Kilograms, 1),new UnitAndPower(Metres, 1),new UnitAndPower(Seconds, -2)),
+                                            new Data("m/s","m⋅s⁻¹", new UnitAndPower(Metres,1), new UnitAndPower(Seconds,-1)),
+                                            new Data("m/s","m⋅s⁻¹", new UnitAndPower(Metres,1), new UnitAndPower(Seconds,-1)),
+                                            new Data("m¹⋅s^-1","m⋅s⁻¹", new UnitAndPower(Metres,1), new UnitAndPower(Seconds,-1)),
+                                            new Data("m^1⋅s^-1","m⋅s⁻¹", new UnitAndPower(Metres,1), new UnitAndPower(Seconds,-1)),
+                                            new Data("m⋅s⁻¹","m⋅s⁻¹", new UnitAndPower(Metres,1), new UnitAndPower(Seconds,-1)),
+                                            new Data("1/s","s⁻¹", new UnitAndPower(Seconds,-1)),
+                                            new Data("s^-1","s⁻¹", new UnitAndPower(Seconds,-1))
                                             //new Data("J/s",new UnitAndPower(Joules, 1),new UnitAndPower(Seconds, -1)),
                                         };
         public IEnumerator GetEnumerator()
         {
             return _datas.GetEnumerator();
         }
+
         public class Data
         {
-            public Data(string value, params UnitAndPower[] units)
+            public Data(string value, string formatted, params UnitAndPower[] units)
             {
                 this.Value = value;
+                this.Formatted = formatted;
                 this.Units = units;
             }
-            public string Value { get; set; }
+
+            public string Value { get; private set; }
+
+            public string Formatted { get; private set; }
+
             public IEnumerable<UnitAndPower> Units { get; private set; }
 
             public override string ToString()
             {
                 var units = string.Join(", ", this.Units.Select(x => string.Format("{0}^{1}", x.Unit.Symbol, x.Power)));
-                return string.Format("String: {0}, Units: {{{1}}}", this.Value, units);
+                return string.Format("{0} Formatted: {1} Units: {{{2}}}", Value, Formatted, units);
             }
         }
     }
