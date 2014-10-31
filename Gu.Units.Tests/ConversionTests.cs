268ff8f71df66c6ab0da8f02fad3b4cd15a867be
Johan Larsson <johan_gavle1@hotmail.com>
2014-10-31 15:50:10 +0100

    Temp, Parser broken but got to go home

---
+++ Gu.Units.Tests/ConversionTests.cs
@@ -0,0 +1,63 @@
+﻿namespace Gu.Units.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    public class ConversionTests
+    {
+        [Test]
+        public void CentimetresMetres()
+        {
+            const double Centimetres = 1.2;
+            var cms = new[]
+                          {
+                              Length.FromCentimetres(Centimetres),
+                              new Length(Centimetres, LengthUnit.Centimetres),
+                              Length.From(Centimetres, LengthUnit.cm)
+                          };
+            foreach (var cm in cms)
+            {
+                Assert.AreEqual(0.012, cm.Metres);
+                var metres = Length.FromMetres(cm.Metres);
+                Assert.AreEqual(Centimetres, metres.Centimetres);
+            }
+        }
+
+        [Test]
+        public void CentimetresMillimetres()
+        {
+            const double Centimetres = 1.2;
+            var cms = new[]
+                          {
+                              Length.FromCentimetres(Centimetres),
+                              new Length(Centimetres, LengthUnit.Centimetres),
+                              Length.From(Centimetres, LengthUnit.cm)
+                          };
+            foreach (var cm in cms)
+            {
+                Assert.AreEqual(12, cm.Millimetres);
+                var millimetres = Length.FromMillimetres(cm.Millimetres);
+                Assert.AreEqual(Centimetres, millimetres.Centimetres);
+            }
+        }
+
+        [TestCaseSource(typeof(ConversionProvider))]
+        public void Convert(ConversionProvider.IConversion<IQuantity> conversion)
+        {
+            Assert.AreEqual(conversion.FromQuantity, conversion.ToQuantity);
+        }
+
+        [TestCaseSource(typeof(UnitsProvider))]
+        public void Roundtrip(IUnit unit)
+        {
+            double[] values = { 0, 100 };
+            foreach (var value in values)
+            {
+                var si = unit.ToSiUnit(value);
+                var d = unit.FromSiUnit(si);
+                Assert.AreEqual(value, d, 1E-9);
+            }
+        }
+    }
+}
--- Gu.Units/Utils/Parser.cs
+++ Gu.Units/Utils/Parser.cs
@@ -2,19 +2,21 @@
 {
     using System;
     using System.Collections.Concurrent;
+    using System.Collections.Generic;
     using System.Globalization;
     using System.Linq;
     using System.Reflection;
+    using System.Text;
     using System.Text.RegularExpressions;
 
     public static class Parser
     {
-        public static readonly string[] Superscripts = { "¹", "²", "³" };
         public static readonly string DoublePointPattern = @"[+-]?\d*(?:.\d+)?(?:[eE][+-]?\d+)?";
         public static readonly string DoubleCommaPattern = @"[+-]?\d*(?:,\d+)?(?:[eE][+-]?\d+)?"; // not super nice :)
         public static readonly string UnitValuePointPattern = string.Format(@"^(?: *)(?<Value>{0}) *(?<Unit>.+) *$", DoublePointPattern);
         public static readonly string UnitValueCommaPattern = string.Format(@"^(?: *)(?<Value>{0}) *(?<Unit>.+) *$", DoubleCommaPattern);
-        private static readonly ConcurrentDictionary<Type, IUnit[]> Symbols = new ConcurrentDictionary<Type, IUnit[]>();
+        private static readonly ConcurrentDictionary<Type, Symbol[]> SymbolCache = new ConcurrentDictionary<Type, Symbol[]>();
+
         public static TValue Parse<TUnit, TValue>(string s, Func<double, TUnit, TValue> creator, IFormatProvider provider = null)
             where TUnit : IUnit
             where TValue : IQuantity
@@ -48,7 +50,7 @@
                 {
                     throw new ArgumentException("Cannot format the given culture", "provider");
                 }
-                double d = ParseDouble(match.Groups["Value"].Value, provider);
+                var d = ParseDouble(match.Groups["Value"].Value, provider);
                 var unit = ParseUnit<TUnit>(match.Groups["Unit"].Value);
                 return creator(d, unit);
             }
@@ -59,11 +61,11 @@
         }
 
         public static TUnit ParseUnit<TUnit>(string s)
+            where TUnit : IUnit
         {
-            var trim = s.Trim();
             var type = typeof(TUnit);
-            var symbols = Symbols.GetOrAdd(type, t => t.Symbol());
-            return (TUnit)symbols.Single(x => x.Symbol == trim);
+            var symbols = SymbolCache.GetOrAdd(type, t => t.Units().Select(x => new Symbol(x)).ToArray());
+            return (TUnit)symbols.Single(x => x.IsMatch(s)).Unit;
         }
 
         public static double ParseDouble(string s, IFormatProvider provider)
@@ -71,7 +73,7 @@
             return double.Parse(s, provider);
         }
 
-        private static IUnit[] Symbol(this Type t)
+        private static IEnumerable<IUnit> Units(this Type t)
         {
             var units = t.GetFields(BindingFlags.GetField | BindingFlags.Public | BindingFlags.Static)
                               .Where(f => typeof(IUnit).IsAssignableFrom(f.FieldType))
@@ -81,32 +83,111 @@
             return units;
         }
 
-        public static int ParsePower(string power)
+        public class Power
         {
-            if (power == "")
+            public Power(int exponent, string[] stringRepresentations)
             {
-                return 1;
+                this.Exponent = exponent;
+                this.StringRepresentations = stringRepresentations;
             }
-            if (power[0] == '⁻')
+            public int Exponent { get; private set; }
+
+            public string Pattern
             {
-                var indexOf = Array.IndexOf(Superscripts, power.Substring(1));
-                if (indexOf < 0)
+                get
                 {
-                    throw new FormatException();
+                    var pattern = string.Join("|", StringRepresentations);
+                    return pattern;
                 }
-                return -1 * (indexOf + 1);
             }
-            int p = Array.IndexOf(Superscripts, power) + 1;
-            if (p > 0)
+
+            public string[] StringRepresentations { get; private set; }
+
+            public override string ToString()
             {
-                return p;
+                var @join = string.Join(", ", StringRepresentations);
+                return string.Format("Exponent: {0}: {{{1}}}", this.Exponent, @join);
             }
-            if (power[0] != '^')
+        }
+
+        public class Symbol
+        {
+            public static readonly Power[] Powers =
+                {
+                    new Power(1,new []{ "¹","^1"}), 
+                    new Power(2,new []{ "²","^2"}), 
+                    new Power(3,new []{ "³","^3"}),
+                    new Power(-1,new []{ "⁻¹","^-1"}), 
+                    new Power(-2,new []{ "⁻²","^-2"}), 
+                    new Power(-3,new []{ "⁻³","^-3"})
+                };
+
+            public static string PowerPattern = string.Join("|", Powers.SelectMany(x => x.StringRepresentations));
+
+            public static string OperatorPattern = @"[⋅*/]";
+            
+            public Symbol(IUnit unit)
             {
-                throw new FormatException();
+                this.Unit = unit;
+                var stringBuilder = new StringBuilder();
+                stringBuilder.Append(@"^ *");
+                var powerMatches = Regex.Matches(unit.Symbol, PowerPattern).OfType<Match>().ToArray();
+                var operatorMatches = Regex.Matches(unit.Symbol, OperatorPattern).OfType<Match>().ToArray();
+                int i = 0;
+                while (i < unit.Symbol.Length)
+                {
+                    var powerMatch = powerMatches.FirstOrDefault(m => m.Index == i);
+                    if (powerMatch != null)
+                    {
+                        var power = Powers.Single(x => x.StringRepresentations.Any(s => s == powerMatch.Value));
+                        stringBuilder.AppendFormat(@"({0})", power.Pattern);
+                        if (power.Exponent == 1)
+                        {
+                            stringBuilder.Append("?");
+                        }
+                        i += powerMatch.Length;
+                        continue;
+                    }
+                    var operatorMatch = operatorMatches.FirstOrDefault(m => m.Index == i);
+                    if (operatorMatch!=null)
+                    {
+                        if (operatorMatch.Value == "/")
+                        {
+                            throw new NotImplementedException("message");
+                        }
+                        else
+                        {
+                            stringBuilder.Append("[⋅*]");
+                            i++;
+                        }
+                        continue;
+                    }
+                    else
+                    {
+                        if (!char.IsWhiteSpace(unit.Symbol[i]))
+                        {
+                            stringBuilder.Append(unit.Symbol[i]);
+                        }
+                        i++;
+                    }
+                }
+                stringBuilder.Append(@" *$");
+                this.Pattern = stringBuilder.ToString();
             }
-            p = int.Parse(power.TrimStart('^'));
-            return p;
+
+            public string Pattern { get; private set; }
+
+            public IUnit Unit { get; private set; }
+
+            public bool IsMatch(string s)
+            {
+                return Regex.IsMatch(s, this.Pattern);
+            }
+
+            public override string ToString()
+            {
+                return String.Format("Unit: {0}, Pattern: {1}", this.Unit, this.Pattern);
+            }
         }
     }
 }
