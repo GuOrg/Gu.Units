54517ae07ffb53bdd6ad43c84481ed3b650ccbc1
Johan Larsson <johan_gavle1@hotmail.com>
2015-11-22 19:41:27 +0100

    Explicit conversions for Time and TimeSpan.

---
+++ Gu.Units/Time.cs
@@ -0,0 +1,17 @@
+namespace Gu.Units
+{
+    using System;
+
+    public partial struct Time
+    {
+        public static explicit operator TimeSpan (Time time)
+        {
+            return TimeSpan.FromSeconds(time.seconds);
+        }
+
+        public static explicit operator Time(TimeSpan timeSpan)
+        {
+            return Time.FromSeconds(timeSpan.TotalSeconds);
+        }
+    }
+}
