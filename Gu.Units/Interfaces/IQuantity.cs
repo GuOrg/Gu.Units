3bfcb1f2b5c8dc1c23ad6e660d9a10080a82a484
Johan Larsson <johan_gavle1@hotmail.com>
2016-02-02 17:32:08 +0100

    Pull up more formatting options to interfaces.

--- Gu.Units/Interfaces/IQuantity.cs
+++ Gu.Units/Interfaces/IQuantity.cs
@@ -5,8 +5,6 @@ namespace Gu.Units
 
     public interface IQuantity : IFormattable, IXmlSerializable
     {
-        Type GetType();
-
         /// <summary>
         /// The value in <see cref="SiUnit"/>
         /// </summary>
@@ -16,5 +14,37 @@ namespace Gu.Units
         /// The <see cref="Gu.Units.IUnit"/> for the <see cref="SiValue"/>
         /// </summary>
         IUnit SiUnit { get; }
+
+        /// <summary>
+        /// Returns a string with the <see cref="Length.SiValue"/> and <see cref="Length.SiUnit"/>
+        /// </summary>
+        /// <returns>The string representation of the <see cref="Length"/></returns>
+        string ToString(IFormatProvider provider);
+
+        /// <summary>
+        /// If an invalid format is provided the string will look like: {value: ??} {unit: ??}
+        /// </summary>
+        /// <param name="format">Must be a composite format ex: \"F2 m\"</param>
+        /// <returns>The string representation of the <see cref="Length"/></returns>
+        string ToString(string format);
+
+        /// <summary>
+        ///  If an invalid format is provided the string will look like: {value: ??} {unit: ??}
+        /// </summary>
+        /// <param name="valueFormat">For formatting the scalar, format stings valid for <see cref="System.Double"/> are valid
+        ///  ex: F2</param>
+        /// <param name="symbolFormat">For formatting of the unit ex m</param>
+        /// <returns>The string representation of the <see cref="Length"/></returns>
+        string ToString(string valueFormat, string symbolFormat);
+
+        /// <summary>
+        ///  If an invalid format is provided the string will look like: {value: ??} {unit: ??}
+        /// </summary>
+        /// <param name="valueFormat">For formatting the scalar, format stings valid for <see cref="System.Double"/> are valid
+        ///  ex: F2</param>
+        /// <param name="symbolFormat">For formatting the unit ex m</param>
+        /// <param name="formatProvider"></param>
+        /// <returns>The string representation of the <see cref="Length"/></returns>
+        string ToString(string valueFormat, string symbolFormat, IFormatProvider formatProvider);
     }
 }
--- Gu.Units/Interfaces/IQuantity{TUnit}.cs
+++ Gu.Units/Interfaces/IQuantity{TUnit}.cs
@@ -1,8 +1,18 @@
 ﻿namespace Gu.Units
 {
+    using System;
+
     public interface IQuantity<in TUnit> : IQuantity
         where TUnit : IUnit
     {
         double GetValue(TUnit unit);
+        string ToString(TUnit unit);
+        string ToString(TUnit unit, SymbolFormat symbolFormat);
+        string ToString(TUnit unit, IFormatProvider formatProvider);
+        string ToString(TUnit unit, SymbolFormat symbolFormat, IFormatProvider formatProvider);
+        string ToString(string valueFormat, TUnit unit);
+        string ToString(string valueFormat, TUnit unit, SymbolFormat symbolFormat);
+        string ToString(string valueFormat, TUnit unit, IFormatProvider formatProvider);
+        string ToString(string valueFormat, TUnit unit, SymbolFormat symbolFormat, IFormatProvider formatProvider);
     }
 }
