b9f6ea4c44bc04a0c7f9cb76bb42d9cedcf94593
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-25 15:19:54 +0200

    wip

---
+++ Gu.Units.Analyzers.Tests/ToLengthCodeFixProviderTests.cs
@@ -0,0 +1,61 @@
+﻿namespace Gu.Units.Analyzers.Tests
+{
+    using System;
+
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    using NUnit.Framework;
+
+    public class ToLengthCodeFixProviderTests : CodeFixVerifier
+    {
+        //Diagnostic and CodeFix both triggered and checked for
+        [Test]
+        public void ChangeToLengthFromMillimetres()
+        {
+            var before = @"
+    namespace ConsoleApplication1
+    {
+        using Gu.Units;
+
+        public class Foo
+        {   
+            public Foo()
+            {
+                Bar = 1;
+            }
+
+            public Length Bar { get; }
+        }
+    }";
+
+            var expected = @"
+    namespace ConsoleApplication1
+    {
+        using Gu.Units;
+
+        public class Foo
+        {   
+            public Foo()
+            {
+                Bar = Length.FromMillimetres(1);
+            }
+
+            public Length Bar { get; }
+        }
+    }";
+            this.VerifyCSharpFix(before, expected);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new ToLengthCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            throw new Exception();
+            //return new TestAnalyzerAnalyzer();
+        }
+    }
+}
---
+++ Gu.Units.Analyzers/ToLengthCodeFixProvider.cs
@@ -0,0 +1,82 @@
+﻿namespace Gu.Units.Analyzers
+{
+    using System.Collections.Immutable;
+    using System.Composition;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeActions;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Simplification;
+
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ToLengthCodeFixProvider)), Shared]
+    public class ToLengthCodeFixProvider : CodeFixProvider
+    {
+        private const string Title = "Length.FromMillimetres(...)";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create("CS0029");
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics.First();
+            var message = diagnostic.GetMessage(null);
+            if (!Regex.IsMatch(message, "Cannot implicitly convert type '(int|double)' to 'Gu.Units.Length'"))
+            {
+                return Task.FromResult(false);
+            }
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(Title, c => ApplyFix(context, c), Title),
+                diagnostic);
+            return Task.FromResult(true);
+        }
+
+        private static async Task<Document> ApplyFix(CodeFixContext context, CancellationToken cancellationToken)
+        {
+            var document = context.Document;
+            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+            // We need to get the LiteralExpressionSyntax that was found by the Analyser
+            var diagnostic = context.Diagnostics.First();
+            var position = diagnostic.Location.SourceSpan.Start;
+            var number = root.FindToken(position)
+                .Parent
+                .AncestorsAndSelf()
+                .OfType<LiteralExpressionSyntax>()
+                .First();
+
+            var replacement = WrapWithCallToMillimetres(number);
+            root = root.ReplaceNode(number, replacement);
+
+            return document.WithSyntaxRoot(root);
+        }
+
+        private static InvocationExpressionSyntax WrapWithCallToMillimetres(ExpressionSyntax expressionToWrap)
+        {
+            return SyntaxFactory.InvocationExpression(
+                InvariantExpressionSyntax,
+                SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(new[] { SyntaxFactory.Argument(expressionToWrap) }))
+                );
+        }
+
+        private static readonly MemberAccessExpressionSyntax InvariantExpressionSyntax =
+            SyntaxFactory.MemberAccessExpression(
+                    SyntaxKind.SimpleMemberAccessExpression,
+                    SyntaxFactory.IdentifierName("Length"),
+                    SyntaxFactory.IdentifierName("FromMillimetres")
+                )
+                .WithAdditionalAnnotations(Simplifier.Annotation);
+    }
+}
