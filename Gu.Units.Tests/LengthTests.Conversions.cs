16b5a7660a46b7ce2090d351d4837f798b1617c3
Johan Larsson <johan_gavle1@hotmail.com>
2017-04-10 18:32:54 +0200

    Amend.

--- Gu.Units.Tests/LengthTests.Conversions.cs
+++ Gu.Units.Tests/LengthTests.Conversions.cs
@@ -1,32 +1,26 @@
 ﻿namespace Gu.Units.Tests
 {
-    using System.Collections.Generic;
     using System.Globalization;
 
     using NUnit.Framework;
 
     public partial class LengthTests
     {
-        public static IReadOnlyList<TestCase> ConversionSource { get; } = new[]
+        [TestCase("25.4 mm", "1 in", true)]
+        [TestCase("0.0254 m", "1 in", true)]
+        [TestCase("12 in", "1 ft", false)]
+        [TestCase("1 ft", "0.3048 m", false)]
+        [TestCase("3 ft", "1 yd", false)]
+        [TestCase("1 m", "100 cm", true)]
+        [TestCase("1 m", "1000 mm", true)]
+        [TestCase("1 km", "1000 m", true)]
+        [TestCase("1 mi", "1.609344 km", true)]
+        [TestCase("1 mi", "1760 yd", true)]
+        public void Conversion(string x, string y, bool exact)
         {
-            new TestCase("25.4 mm", "1 in"),
-            new TestCase("0.0254 m", "1 in"),
-            new TestCase("12 in", "1 ft", false),
-            new TestCase("1 ft", "0.3048 m", false),
-            new TestCase("3 ft", "1 yd", false),
-            new TestCase("1 m", "100 cm"),
-            new TestCase("1 m", "1000 mm"),
-            new TestCase("1 km", "1000 m"),
-            new TestCase("1 mi", "1.609344 km"),
-            new TestCase("1 mi", "1760 yd"),
-        };
-
-        [TestCaseSource(nameof(ConversionSource))]
-        public void Conversion(TestCase data)
-        {
-            var l1 = Length.Parse(data.X, CultureInfo.InvariantCulture);
-            var l2 = Length.Parse(data.Y, CultureInfo.InvariantCulture);
-            if (data.Exactly)
+            var l1 = Length.Parse(x, CultureInfo.InvariantCulture);
+            var l2 = Length.Parse(y, CultureInfo.InvariantCulture);
+            if (exact)
             {
                 Assert.AreEqual(l1, l2);
                 Assert.AreEqual(l2, l1);
@@ -35,30 +29,6 @@
             {
                 Assert.AreEqual(l1.ToString(), l2.ToString());
                 Assert.AreEqual(l2.ToString(), l1.ToString());
-            }
-        }
-
-        public class TestCase
-        {
-            public TestCase(string x, string y, bool exactly = true)
-            {
-                this.X = x;
-                this.Y = y;
-                this.Exactly = exactly;
-            }
-
-            public string X { get; }
-
-            public string Y { get; }
-
-            public bool Exactly { get; }
-
-            public override string ToString()
-            {
-                var equalitySymbol = this.Exactly
-                    ? "=="
-                    : "~";
-                return $"{this.X} {equalitySymbol} {this.Y}";
             }
         }
     }
