310fb76c3769da8086f6c7ba5220073e34cc8243
Johan Larsson <johan_gavle1@hotmail.com>
2017-05-06 00:44:23 +0200

    Dump missing quantities.

---
+++ Gu.Units.Generator.Tests/Scripts/DumpMissing.cs
@@ -0,0 +1,20 @@
+﻿namespace Gu.Units.Generator.Tests.Scripts
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+
+    public class DumpMissing
+    {
+        [Test]
+        public void Run()
+        {
+            foreach (var overloads in Settings.Instance.Missing.Where(x => x.Parts.All(p => p.Power < 5))
+                                                               .Where(x => x.Missing.Count > 1)
+                                                               .OrderByDescending(x => x.Missing.Count))
+            {
+                Console.WriteLine($"{overloads.Parts} {overloads.Missing.Count}");
+            }
+        }
+    }
+}
