50738cc7689d940481b3b7d2083fbf2f2d23caab
JohanLarsson <johan_gavle1@hotmail.com>
2020-10-30 09:25:52 +0100

    Use new csproj and nuget

--- Gu.Units.Wpf/AssemblyAttributes.cs
+++ Gu.Units.Wpf/AssemblyAttributes.cs
@@ -1,25 +1,8 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 using System.Windows.Markup;
 
-[assembly: AssemblyTitle("Gu.Units.Wpf")]
-[assembly: AssemblyDescription("IValueConverters for Gu.Units as MarkupExtensions")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Johan Larsson")]
-[assembly: AssemblyProduct("Gu.Units.Wpf")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("ea4bce51-a58d-4fa7-bba4-6f0056d753ca")]
-[assembly: AssemblyVersion("0.10.6.1")]
-[assembly: AssemblyFileVersion("0.10.6.1")]
 [assembly: InternalsVisibleTo("Gu.Units.Wpf.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100A1B1D134D8F3AE3E10A5A45C809C092F9F43A1A87A1C7F59E06BEF6C0BB35D9ED93A37A9A6D182D04A813635120A17DEADBB5A9D64B436FF98F02FAFE8E0069E16878EC48C4161A1E063EED9BC12A5BB92F225745D155C1FE549B2F679DFCBA0094B36B0ACE4BFB79412BE10DBBE6C8F72516C1D2FD1A7FBF356E56E7AAA29BC", AllInternalsVisible = true)]
-
 [assembly: XmlnsDefinition("http://Gu.com/Units", clrNamespace: "Gu.Units", AssemblyName = "Gu.Units")]
 [assembly: XmlnsDefinition("http://Gu.com/Units", clrNamespace: "Gu.Units.Wpf", AssemblyName = "Gu.Units.Wpf")]
 [assembly: XmlnsDefinition("http://Gu.com/Units", clrNamespace: "Gu.Units.Wpf.Proxies", AssemblyName = "Gu.Units.Wpf")]
 [assembly: XmlnsPrefix("http://Gu.com/Units", "units")]
-[assembly: NeutralResourcesLanguage("en")]
