20cb88a081fa61704576ce8f5db32f478e79c25d
JohanLarsson <johan_gavle1@hotmail.com>
2020-11-20 10:13:27 +0100

    Simplify

--- Gu.Units.Generator/Views/ObservableExtensions.cs
+++ Gu.Units.Generator/Views/ObservableExtensions.cs
@@ -4,13 +4,13 @@
     using System.Reactive.Linq;
     using System.Threading.Tasks;
 
-    public static class MyObservableExtensions
+    public static class ObservableExtensions
     {
 #pragma warning disable VSTHRD200 // Use "Async" suffix for async methods
         public static IDisposable SubscribeAsync<T>(this IObservable<T> source, Func<Task> onNextAsync) =>
 #pragma warning restore VSTHRD200 // Use "Async" suffix for async methods
             source
-                .Select(number => Observable.FromAsync(onNextAsync))
+                .Select(_ => Observable.FromAsync(onNextAsync))
                 .Concat()
                 .Subscribe();
     }
