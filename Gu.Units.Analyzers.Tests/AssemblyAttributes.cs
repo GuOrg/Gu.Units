29edc47071f0fb493ede25c8e2726a4571e929fe
JohanLarsson <johan_gavle1@hotmail.com>
2021-01-05 19:09:05 +0100

    [assembly: CLSCompliant(false)]

--- Gu.Units.Analyzers.Tests/AssemblyAttributes.cs
+++ Gu.Units.Analyzers.Tests/AssemblyAttributes.cs
@@ -1,4 +1,7 @@
-﻿using Gu.Roslyn.Asserts;
+﻿using System;
+using Gu.Roslyn.Asserts;
+
+[assembly: CLSCompliant(false)]
 
 [assembly: MetadataReference(typeof(object), new[] { "global", "mscorlib" })]
 [assembly: MetadataReference(typeof(System.Diagnostics.Debug), new[] { "global", "System" })]
